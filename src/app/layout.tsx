d21b89f7acfd297ba57babd4b1e6508f487c4743
Mohamed Sallam <mohamed.sallam@learnit.fi>
2023-10-25 09:31:31 +0300

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,33 @@ import { cn, constructMetadata } from '@/lib/utils'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Providers from '@/components/providers';
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "Chat With PDF's",
-//   description: "Smart AI Chat With PDF's Files",
-// };
-export const metadata = constructMetadata()
+// ##################
+export const metadata: Metadata = {
+  title: "Chat With PDF's",
+  description: "Smart AI Chat With PDF's Files",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.png",
+        href: "/logo.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo.png",
+        href: "/logo.png",
+      },
+    ],
+  },
+};
+// ##################
+// export const metadata = constructMetadata()
+// ##################
+
 
 
 export default function RootLayout({
